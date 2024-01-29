2a5e6d3f1d3bf87c9e2519ce900fbddf2d4240fe
ChristianBaciu <christian3110@outlook.it>
2024-01-29 19:08:41 +0100

    funzione aggiungi task

--- main.js
+++ main.js
@@ -2,13 +2,23 @@ const{createApp}= Vue
 createApp({
     data(){
         return{
-            
+            newTask: '',
+            task: [
+                'fare ecc...',
+                'fare ecc...',
+                'fare ecc...',
+            ]
         }
     },
     created(){
 
     },
     methods:{
+        // unshift aggiunge al primo posto una nuova task
+        addTask(){
+            this.task.unshift(this.newTask)
+        }   // push aggiunge in fondo posto una nuova task
+
 
     }
 }).mount('#app')
